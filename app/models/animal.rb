41f192c41c9250e46fb4470457d8fab73d6a5a13
Marilyn Remigio <marilynremigio@gmail.com>
2023-03-31 12:50:20 -0600

    adds accepts_nested_attributes_for to create new animal with sightings in a single API request, completes story 5

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,5 +1,7 @@
 class Animal < ApplicationRecord
     has_many :sightings
+    accepts_nested_attributes_for :sightings
+
     validates :common_name, :scientific_binomial, presence: true
     validates :common_name, :scientific_binomial, uniqueness: true
 end
