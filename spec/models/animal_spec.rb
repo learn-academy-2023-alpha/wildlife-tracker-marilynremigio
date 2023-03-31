241ae8bd516249cb69664e99edd683ed9f2df6dd
Marilyn Remigio <marilynremigio@gmail.com>
2023-03-31 12:09:17 -0600

    validates presence of attributes, uniqueness, completes story 4

--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Animal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'is not valid without a common name' do
+    animal = Animal.create(scientific_binomial: 'Panthera leo')
+    expect(animal.errors[:common_name]).to_not be_empty
+  end
+
+  it 'is not valid without a scientific binomial' do
+    animal = Animal.create(common_name: 'Lion')
+    expect(animal.errors[:scientific_binomial]).to_not be_empty
+  end
+
+  it 'is not valid if common name and scientific bionomial are the same' do
+    animal = Animal.create(common_name: 'Lion', scientific_binomial: 'Lion')
+    expect(animal.errors[:common_name]).to_not be_empty
+  end 
+
+  it 'not valid if common name and scientific binomial are not unique' do
+    animal1 = Animal.create(common_name: 'Lion', scientific_binomial: 'Panthera leo')
+    animal2 = Animal.create(common_name: 'Lion', scientific_binomial: 'Panthera leo')
+    expect(animal2).to_not be_valid
+  end
+
 end
--- spec/models/sighting_spec.rb
+++ spec/models/sighting_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Sighting, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'is not valid without a latitude' do
+    sighting = Sighting.create(longitude: '-104.821336', date: '2023/03/06')
+    expect(sighting.errors[:latitude]).to_not be_empty
+  end
+
+  it 'is not valid without a longitude' do
+    sighting = Sighting.create(latitude: '38.833487', date: '2023/03/06')
+    expect(sighting.errors[:longitude]).to_not be_empty
+  end
+
+  it 'is not valid without a date' do
+    sighting = Sighting.create(latitude: '38.833487', longitude: '-104.821336')
+    expect(sighting.errors[:date]).to_not be_empty
+  end
+  
 end
